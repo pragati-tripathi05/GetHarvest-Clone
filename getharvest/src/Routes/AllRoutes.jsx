a63aae0236ef6bf37701afb88065ea9857bf3531
pragati tripathi <mailpragatihere@gmail.com>
2022-07-20 06:12:01 +0530

    cra- structure

---
+++ getharvest/src/Routes/AllRoutes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePage from "../Pages/HomePage";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      AllRoutes
+      <HomePage />
+    </div>
+  );
+};
+
+export default AllRoutes;
